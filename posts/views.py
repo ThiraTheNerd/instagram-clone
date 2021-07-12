2d12dd1d593841e743ff7f2b65e89ee7a7ca846e
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-12 11:07:23 +0300

    add the url for the images

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from account.models import Profile
 
 # Create your views here.
 # Create your views here.
 @login_required(login_url="/accounts/login/")
 def index(request):
     posts = Post.objects.all()
-
-    return render(request, "index.html", {"posts": posts})
+    users = Profile.objects.all()
+    return render(request, "index.html", {"posts": posts, "users": users})
