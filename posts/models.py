b409573f44710ca9ba487ff529913da946559d19
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-14 06:20:49 +0300

    add the comment model

--- posts/models.py
+++ posts/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
+from .models import Post
 
 # Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=CASCADE, related_name="comments")
+    created_on = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.post
+
+
 class Post(models.Model):
     image = models.ImageField(upload_to="posts/")
     caption = HTMLField()
     user = models.OneToOneField(User, on_delete=CASCADE)
     post_date = models.DateTimeField(auto_now_add=True)
+    comments = models.ManyToManyField(Comment)
 
     def __str__(self):
         return f"{self.id}_{self.post_date}_by_{self.user.username}"
