a7519aed3ba79c0fb67db1e1ac24910f41c05a9a
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-15 10:02:16 +0300

    change tinymce version

--- instagram/settings.py
+++ instagram/settings.py
@@ -156,6 +156,8 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 # configuring the location for media
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
