25c1a1e8360e679415f6abc8f09cf925bc90efc9
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-15 10:23:06 +0300

    change accounts url

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url("^$", views.index, name="index"),
+    url("", views.index, name="index"),
     url(r"^post/(?P<pk>\d+)", views.view_post, name="view_post"),
     url("search/", views.search_results, name="search_results"),
     url(r"new-comment/(?P<post_id>\d+)", views.new_comment, name="new_comment"),
